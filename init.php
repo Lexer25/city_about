5cb413cf219550985b4e7f494eeda871c67bfebb
Andre <andrey.buharov@gmail.com>
2026-04-24 13:01:08 +0300

    1.0.2 Удалил упоминания о старых версиях и изменения.

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 // MODPATH/about/init.php
-defined('ABOUT_VERSION') OR define('ABOUT_VERSION', '1.0.0');
+defined('ABOUT_VERSION') OR define('ABOUT_VERSION', '1.0.2');
 
 // Дополнительная инициализация модуля About
 // Можно добавить загрузку дополнительных файлов если нужно
