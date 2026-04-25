9bac4824a4f6457b509fd34a117bf1b387e8d36d
Андрей <andrey.buharov@gmail.com>
2026-04-25 08:24:40 +0300

    1.0.3 ДОбавил нумерацию строк и отключение показа модулей Kohana

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 // MODPATH/about/init.php
-defined('ABOUT_VERSION') OR define('ABOUT_VERSION', '1.0.2');
+defined('ABOUT_VERSION') OR define('ABOUT_VERSION', '1.0.3');
 
 // Дополнительная инициализация модуля About
 // Можно добавить загрузку дополнительных файлов если нужно
